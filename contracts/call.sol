60e0e7c45bf61833f439e836eb48dcb458e6dda0
Dylan <dylanwong007@gmail.com>
2022-11-05 09:44:20 -0800

    Update call.sol

--- contracts/call.sol
+++ contracts/call.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.9;
 
 contract Caller {
 // f0 declaration
-    Counter public myFZero;
+    ExampleSoliditySprint2022 public myFZero;
 
 
 
 
     constructor(address counterAddress) {
 // call to f0
-        myFZero = Counter(counterAddress);
+        myFZero = ExampleSoliditySprint2022(counterAddress);
 
     }
 
