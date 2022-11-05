876aaf777c6dacc9a9de9fa27e0f81314b7208fb
Dylan <dylanwong007@gmail.com>
2022-11-05 09:35:14 -0800

    Update call.sol

--- contracts/call.sol
+++ contracts/call.sol
@@ -2,34 +2,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract Callee {
-    uint public x;
-    uint public value;
-
-    function setX(uint _x) public returns (uint) {
-        x = _x;
-        return x;
-    }
-
-    function setXandSendEther(uint _x) public payable returns (uint, uint) {
-        x = _x;
-        value = msg.value;
-
-        return (x, value);
-    }
-}
 
 contract Caller {
-    function setX(Callee _callee, uint _x) public {
-        uint x = _callee.setX(_x);
+// f0 declaration
+    Counter public myFZero;
+
+
+
+
+    constructor(address counterAddress) {
+// call to f0
+        myFZero = Counter(counterAddress);
+
     }
 
-    function setXFromAddress(address _addr, uint _x) public {
-        Callee callee = Callee(_addr);
-        callee.setX(_x);
+
+
+
+    function callFZero() external {
+
+        myFZero.f0(0);
+
     }
 
-    function setXandSendEther(Callee _callee, uint _x) public payable {
-        (uint x, uint value) = _callee.setXandSendEther{value: msg.value}(_x);
-    }
 }
