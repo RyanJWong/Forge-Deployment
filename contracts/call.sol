0b1b9768b2e3c1e67551adc0daa0b166487e3928
Dylan <dylanwong007@gmail.com>
2022-11-05 11:01:14 -0800

    Update call.sol

--- contracts/call.sol
+++ contracts/call.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.8.9;
 
 contract Create2Contract {
-    bytes public constant getRekt = "bet you can't guess my address before im even deployed";
-    constructor() {
-
-    }
+function requestData() public returns (bytes32 requestId)
+{
+   Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+   request.add("get", "https://api.github.com/fruitbox12");
+   request.add("path", "status");    
+   return sendChainlinkRequestTo(oracle, request, fee);
+}
+function fulfill(bytes32 _requestId, uint256 _response) public recordChainlinkFulfillment(_requestId)
+{
+   response = _response;
+}
 }
