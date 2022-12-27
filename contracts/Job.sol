b39ee4c9a746d58349a3f72380f2e4662d38be40
Dylan <dylanwong007@gmail.com>
2022-12-27 15:05:11 -0800

    Update Job.sol

--- contracts/Job.sol
+++ contracts/Job.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.0 || ^0.6.0 || ^0.7.0 || ^0.8.4;
+
 
 import "@ensdomains/ens/contracts/ENSRegistry.sol";
 import "@ensdomains/ens/contracts/FIFSRegistrar.sol";
