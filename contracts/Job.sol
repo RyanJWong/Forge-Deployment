110521e1365ee8273bdd62fe9ade467b3973f6fc
Dylan <dylanwong007@gmail.com>
2022-12-26 22:21:06 -0800

    Update Job.sol

--- contracts/Job.sol
+++ contracts/Job.sol
@@ -1,132 +1,47 @@
-pragma solidity 0.8.15; 
+pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Strings.sol"; 
-import "@openzeppelin/contracts/access/Ownable.sol"; 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@ensdomains/ens/contracts/ENSRegistry.sol";
+import "@ensdomains/ens/contracts/FIFSRegistrar.sol";
+import "@ensdomains/ens/contracts/ReverseRegistrar.sol";
+import "@ensdomains/resolver/contracts/PublicResolver.sol";
 
+// Construct a set of test ENS contracts.
+contract ZapDependencies {
+  bytes32 constant TLD_LABEL = keccak256("zap");
+  bytes32 constant RESOLVER_LABEL = keccak256("resolver");
+  bytes32 constant REVERSE_REGISTRAR_LABEL = keccak256("reverse");
+  bytes32 constant ADDR_LABEL = keccak256("addr");
 
-contract WebRTC is ERC1155, Ownable {
-    using Strings for uint256; 
+  ENSRegistry public ens;
+  FIFSRegistrar public fifsRegistrar;
+  ReverseRegistrar public reverseRegistrar;
+  PublicResolver public publicResolver;
 
-    struct InitialParameters {
-        string name; 
-        string symbol; 
-        string uri; 
-    }
+  function namehash(bytes32 node, bytes32 label) public pure returns (bytes32) {
+    return keccak256(abi.encodePacked(node, label));
+  }
 
+  constructor() public {
+    ens = new ENSRegistry();
+    publicResolver = new PublicResolver(ens);
 
-uint16 public maxAllowed; 
-uint256 public price;
+    // Set up the resolver
+    bytes32 resolverNode = namehash(bytes32(0), RESOLVER_LABEL);
 
-string private baseURI; 
-string public name; 
-string public symbol;
+    ens.setSubnodeOwner(bytes32(0), RESOLVER_LABEL, address(this));
+    ens.setResolver(resolverNode, address(publicResolver));
+    publicResolver.setAddr(resolverNode, address(publicResolver));
 
-bool public investingAssetsLocked = false;
-bool public assetMint = false; 
+    // Create a FIFS registrar for the TLD
+    fifsRegistrar = new FIFSRegistrar(ens, namehash(bytes32(0), TLD_LABEL));
 
-address public ens;
+    ens.setSubnodeOwner(bytes32(0), TLD_LABEL, address(fifsRegistrar));
 
-mapping(uint256 => bool) public investedAsset;
-mapping(address => uint256) public mintedBalance;
+    // Construct a new reverse registrar and point it at the public resolver
+    reverseRegistrar = new ReverseRegistrar(ens, NameResolver(address(publicResolver))); 
 
-event SetBaseURI(string indexed _uri); 
-
-constructor(
-    address _owner, 
-    InitialParameters memory initialParameters)
-    ERC1155(initialParameters.uri) {
-        name  = initialParameters.name;
-        symbol = initialParameters.symbol;
-        baseURI = initialParameters.uri; 
-        emit SetBaseURI(baseURI); 
-        transferOwnership(_owner); 
-        setERC721HERE("flex.eth");
-    }
-    //
-    // @dev: locks the assets forever, you can not revert this.
-    //
-
-    function lockAssets() public onlyOwner {
-        investingAssetsLocked = true; 
-    }
-    // 
-    // @dev: creates new asset for airdropping / minting
-    //
-
-    function newAsset(uint256 _id) public onlyOwner {
-        require(!investingAssetsLocked, "The assets are locked and no more can be created"); 
-        require(investedAsset[_id] == false, "This ID already has an asset created under it, try another number"); 
-        investedAsset[_id] = true; 
-    }
-    //
-    // @dev: Mint function for only BRAQFRND holders.
-    //
-    function mint(uint256 _id, uint256 amount) public payable {
-        require(!investingAssetsLocked, "The assets are locked and no more can be minted"); 
-        require(assetMint, "Mint is not open for holders to mint more");
-        IERC721 ENS = IERC721(tokenname);
-        uint256 amountToken = tokenname.balanceOf(msg.sender);
-        require(amountToken >= 1, "You are not a holder"); 
-        uint256 mintedCount = mintedBalance[msg.sender]; 
-        require(mintedCount + amount <= maxAllowed, "Max Fractions minted"); 
-        require(msg.value == price * amount, "Not enough ETH to complete tx"); 
-
-        mintedBalance[msg.sender]++; 
-        
-        _mint(msg.sender, _id, amount, ""); 
-    }
-    //
-    // @dev: Owner function to mint several assets at once for airdrop.
-    //
-    function mintBatch(uint256[] memory _ids, uint256[] memory _quantity) external onlyOwner {
-        require(!investingAssetsLocked, "The assets are locked and no more can be minted"); 
-        
-        _mintBatch(owner(), _ids, _quantity, ""); 
-    }
-    //
-    // @dev: Function to change the max allowed of mints for this round.
-    //
-    function changeMaxAllowed(uint16 _maxAllowed) public onlyOwner {
-        maxAllowed = _maxAllowed; 
-    }
-    //
-    // @dev: Function to allow holders to mint additional fractions.
-    //
-    function changeMintState() external onlyOwner {
-        assetMint = !assetMint; 
-    }
-    //
-    // @dev: Function to change the price of current asset to mint.
-    //
-    function setPrice(uint256 _price) public onlyOwner {
-        price = _price; 
-    }
-    //
-    // @dev: Function to set contract of BRAQ
-    //
-    function setERC721HERE(address _newAddress) public onlyOwner {
-        ens = _newAddress;
-    }
-    //
-    // @dev: function to change the URI in the occurence of new assets being added. 
-    //
-    function setURI(string memory _uri) external onlyOwner {
-        baseURI = _uri;
-        emit SetBaseURI(baseURI); 
-    }
-    //
-    // @dev: concatenation of the URI to display on Opensea depending on token. 
-    //
-    function uri(uint256 _id) public view override returns (string memory) {
-        require(investedAsset[_id], "URI requested for invalid asset."); 
-        return bytes(baseURI).length > 0
-        ? string(abi.encodePacked(baseURI, _id.toString(), ".json"))
-        : baseURI; 
-    }
-    function withdrawETH() public onlyOwner {
-        (bool os, ) = payable(owner()).call{value: address(this).balance}('');
-        require(os); 
-    }
+    // Set up the reverse registrar
+    ens.setSubnodeOwner(bytes32(0), REVERSE_REGISTRAR_LABEL, address(this));
+    ens.setSubnodeOwner(namehash(bytes32(0), REVERSE_REGISTRAR_LABEL), ADDR_LABEL, address(reverseRegistrar));
+  }
 }
