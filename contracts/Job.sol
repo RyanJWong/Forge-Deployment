3890eec524a97914d9a62fa8d65d650e3078e6a1
Dylan <dylanwong007@gmail.com>
2022-12-26 19:11:59 -0800

    Update Job.sol

--- contracts/Job.sol
+++ contracts/Job.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // This contract extends the ERC1155 contract from OpenZeppelin to add a namespace and fractionalization
 // functionality.
