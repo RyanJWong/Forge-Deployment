d839610d17d89114463d805e40c43bcee121c5d3
Dylan <dylanwong007@gmail.com>
2022-12-26 18:49:46 -0800

    Update Job.sol

--- contracts/Job.sol
+++ contracts/Job.sol
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts/utils/Address.sol";
 contract WebRTC is ERC1155 {
   using SafeMath for uint256;
   using Address for address;
+  address private _owner;
 
   // The namespace for this token.
   string private _namespace;
@@ -17,8 +18,8 @@ contract WebRTC is ERC1155 {
   // Mapping from token ID to the fractionalization factor for that token.
   mapping(uint256 => uint256) private _fractionalizationFactors;
 
-  // Mapping from token ID to the address of the ERC20 contract for that token.
-  mapping(uint256 => address) private _erc20Contracts;
+  // Mapping from token ID to the balance of the ERC20 token for that token.
+  mapping(uint256 => uint256) private _erc20Balances;
 
   // The constructor sets the namespace and owner of the contract.
   constructor(string memory namespace) public {
@@ -38,13 +39,6 @@ contract WebRTC is ERC1155 {
     _fractionalizationFactors[tokenId] = factor;
   }
 
-  // Sets the ERC20 contract address for a given token ID. Only the contract owner can do this.
-  function setERC20Contract(uint256 tokenId, address contractAddress) public {
-    require(_owner == msg.sender, "Only the contract owner can set the ERC20 contract address.");
-    require(Address(contractAddress).isContract(), "The contract address must be a contract.");
-    _erc20Contracts[tokenId] = contractAddress;
-  }
-
   // Mints a new ERC1155 token and assigns it the specified namespace.
   function mint(address[] memory to, uint256[] memory tokenIds, uint256[] memory amounts) public {
     require(_owner == msg.sender, "Only the contract owner can mint new tokens.");
@@ -53,21 +47,20 @@ contract WebRTC is ERC1155 {
 
   // Allows a user to exchange a fraction of an ERC1155 token for an equivalent amount of the corresponding
   // ERC20 token.
- // Allows a user to exchange a fraction of an ERC1155 token for an equivalent amount of the corresponding
-// ERC20 token.
-function exchange(uint256 tokenId, uint256 amount) public {
-  // Ensure that the fractionalization factor and ERC20 contract address have been set for this token.
-  require(_fractionalizationFactors[tokenId] > 0, "The fractionalization factor or ERC20 contract address has not been set for this token.");
+  function exchange(uint256 tokenId, uint256 amount) public {
+    // Ensure that the fractionalization factor has been set for this token.
+    require(_fractionalizationFactors[tokenId] > 0, "The fractionalization factor has not been set for this token.");
 
-  // Calculate the equivalent amount of the ERC20 token.
-  uint256 equivalentAmount = amount.mul(_fractionalizationFactors[tokenId]);
+    // Calculate the equivalent amount of the ERC20 token.
+    uint256 equivalentAmount = amount.mul(_fractionalizationFactors[tokenId]);
 
-  // Transfer the equivalent amount of the ERC20 token to the caller.
-  ERC20 erc20 = ERC20(_erc20Contracts[tokenId]);
-  erc20.safeTransfer(msg.sender, equivalentAmount);
+    // Transfer the equivalent amount of the ERC20 token to the caller.
+    _erc20Balances[tokenId] = _erc20Balances[tokenId].sub(equivalentAmount);
+    msg.sender.transfer(equivalentAmount);
 
-  // Burn the corresponding amount of the ERC1155 token.
-  burn(msg.sender, tokenId, amount);
-}
+    // Burn the corresponding amount of the ERC1155 token.
+    burn(msg.sender, tokenId, amount);
+  }
+
 
 }
