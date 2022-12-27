df4654ef225db6139044bcd3b8391ef71bdd706c
Dylan <dylanwong007@gmail.com>
2022-12-26 17:56:08 -0800

    Update Job.sol

--- contracts/Job.sol
+++ contracts/Job.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/utils/Address.sol";
+import "@openzeppelin/math/Address.sol";
 import "@openzeppelin/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/utils/Address.sol";
 
