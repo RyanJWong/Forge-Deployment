a7a395cc8bbcf1714b91fdad8082bbea9536b56f
fruitbox12 <dylanwong007@gmail.com>
2023-04-17 19:12:06 -0700

    Update TheHerd.sol

--- contracts/TheHerd.sol
+++ contracts/TheHerd.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721.sol";
 import "@openzeppelin/contracts-upgradeable/access/Ownable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/security/Pausable.sol";
